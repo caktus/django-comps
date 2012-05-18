77c3bb96b7eb0bfff7af871a6db849e3c12a3dad
David Ray <davidray@gmail.com>
2012-05-17 21:40:47 -0400

    packaging fixes

---
+++ comps/__init__.py
@@ -0,0 +1,25 @@
+"""
+A simple application that provides an entry point for integrating front end designers into a django project
+"""
+
+
+__version_info__ = {
+    'major': 0,
+    'minor': 1,
+    'micro': 0,
+    'releaselevel': 'final',
+}
+
+def get_version():
+    """
+Return the formatted version information
+"""
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final':
+        vers.append('%(releaselevel)s' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
