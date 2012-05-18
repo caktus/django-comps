5877b50da29ad97a4f0d92edb85dd6ccba1723a7
David Ray <davidray@gmail.com>
2012-05-17 21:49:02 -0400

    fix bad urlconf

--- comps/urls.py
+++ comps/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^comps$', 'comp_listing', name='comp-listing'),
+    url(r'^comps/$', 'comp_listing', name='comp-listing'),
     url(r'^comps/(?P<slug>[\w.\-]+)$', 'comp', name='comp'),
 )
