08c5821be985cffd86f7a9bc66f0a45a9e2abf97
David Ray <davidray@gmail.com>
2012-07-04 14:41:54 -0400

    Work on #1

--- comps/urls.py
+++ comps/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('comps.views',
-    url(r'^comps/$', 'comp_listing', name='comp-listing'),
-    url(r'^comps/(?P<slug>[\w.\-]+)$', 'comp', name='comp'),
+    url(r'^comps(?:/(?P<directory_slug>[\w\-]+))?/$', 'comp_listing', name='comp-listing'),
+    url(r'^comps(?:/(?P<directory_slug>[\w\-]+))?/(?P<slug>[\w.\-]+)$', 'comp', name='comp'),
 )
--- comps/views.py
+++ comps/views.py
@@ -5,19 +5,28 @@ from django.shortcuts import redirect, render
 from django.template import TemplateDoesNotExist
 
 
-def comp_listing(request):
+def comp_listing(request, directory_slug=None):
     context = {}
-    templates = os.listdir(settings.COMPS_DIR)
+    working_dir = settings.COMPS_DIR
+    if directory_slug:
+        working_dir = os.path.join(working_dir, directory_slug)
+    templates = os.listdir(working_dir)
     listing = 'comp_listing.html'
     templates = [x for x in templates if x != listing]
     templates.sort()
     context['templates'] = templates
+    context['subdirectory'] = directory_slug
     return render(request, "comps/{0}".format(listing), context)
 
 
-def comp(request, slug):
+def comp(request, slug, directory_slug=None):
     context = {}
     template = "comps/{0}".format(slug)
+    if directory_slug:
+        template = "comps/{0}/{1}".format(directory_slug, slug)
+    working_dir = os.path.join(settings.COMPS_DIR, slug)
+    if os.path.isdir(working_dir):
+        return redirect('comp-listing', directory_slug=slug)
     try:
         return render(request, template, context)
     except TemplateDoesNotExist:
