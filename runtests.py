76ffd5a1e3b52be442e178d241d4dabebe768380
David Ray <davidray@gmail.com>
2012-09-07 18:47:56 -0400

    Moving toward tox

---
+++ runtests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import sys
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'comps',
+        ),
+        SITE_ID=1,
+        SECRET_KEY='super-secret',
+        TEMPLATE_CONTEXT_PROCESSORS=(
+            'django.contrib.auth.context_processors.auth',
+            'django.core.context_processors.request',
+        ),
+        ROOT_URLCONF='comps.tests.urls',
+        PASSWORD_HASHERS=(
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+        ),
+    )
+
+
+from django.test.utils import get_runner
+
+
+def runtests():
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+    failures = test_runner.run_tests(['comps', ])
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests()
