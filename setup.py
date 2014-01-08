d3aa3ee4f838fcadafc2dc2070d1b33f8ae03590
Caleb Smith <caleb.smithnc@gmail.com>
2014-01-08 11:43:35 -0500

    Updated setup.py and AUTHORS with more recent info

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ def read_file(filename):
 setup(
     name='django-comps',
     version=__import__('comps').__version__,
-    author='David Ray',
-    author_email='davidray@gmail.com',
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/daaray/django-comps',
+    url='https://github.com/caktus/django-comps',
     license='BSD',
     description=u' '.join(__import__('comps').__doc__.splitlines()).strip(),
     classifiers=[
